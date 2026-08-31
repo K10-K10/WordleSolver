ca0e6e2b678e19ff66668e0313afd1b21168bb19
K10-K10 <keito.tottori@gmail.com>
2026-09-01 00:29:00 +0900

    update: class

--- include/suggest.h
+++ include/suggest.h
@@ -9,20 +9,27 @@
 
 namespace wordle {
 enum class type { hit, blow, miss };
-void input(std::string word, std::array<type, 5> result);
-std::string suggest();
-}  // namespace wordle
 
-namespace _wordle_ {
-int init();
-inline int size;
-void update_list();
-inline std::vector<std::string> list;
-inline std::vector<std::string> suggest_list;
-inline std::unordered_set<char> miss;
-inline std::vector<std::array<bool, 6>> blow(26, std::array<bool, 6>{false});
-// is_blow, 0,1,2,3,4
-inline std::vector<char> correct(5, ' ');
-}  // namespace _wordle_
+class WordleSolver {
+ public:
+  WordleSolver();
+  void co_list();
+  void input(std::string word, std::array<type, 5> result);
+  std::string suggest();
+
+ private:
+  int init();
+  void update_list();
+  int size = 0;
+  std::vector<std::string> list;
+  std::vector<std::string> suggest_list;
+  std::unordered_set<char> miss;
+  std::vector<std::array<bool, 6>> blow =
+      std::vector<std::array<bool, 6>>(26, std::array<bool, 6>{false});
+  // is_blow, 0,1,2,3,4
+  std::unordered_set<char> blow_cnt;
+  std::vector<char> correct = std::vector<char>(5, ' ');
+};
+}  // namespace wordle
 
 #endif
