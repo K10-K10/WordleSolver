9b02a2de74d54286214f60a1e4b86da2c7c1a7fe
K10-K10 <keito.tottori@gmail.com>
2026-08-31 22:45:52 +0900

    fix: add inline keyword

--- include/suggest.h
+++ include/suggest.h
@@ -1,30 +1,27 @@
 #ifndef INCLUDE_SUGGEST_H_
 #define INCLUDE_SUGGEST_H_
 
-#include <vector>
-#include <string>
 #include <array>
 #include <map>
+#include <string>
 #include <unordered_set>
+#include <vector>
 
-namespace wordle{
-  enum class type{
-    hit,
-    blow,
-    none
-  };
-  void input(std::string word,std::array<type,5 > result);
-  std::string suggest();
-}
+namespace wordle {
+enum class type { hit, blow, none };
+void input(std::string word, std::array<type, 5> result);
+std::string suggest();
+}  // namespace wordle
 
-namespace _wordle_{
-  int init();
-  int size;
-  std::vector<std::string> list;
-  std::vector<std::string> suggest_list;
-  std::unordered_set<char> miss;
-  std::vector<std::vector<char>> blow(5,std::vector<char>());// set un correct index;
-  std::vector<char> correct(5);
-}
+namespace _wordle_ {
+int init();
+inline int size;
+void update_list();
+inline std::vector<std::string> list;
+inline std::vector<std::string> suggest_list;
+inline std::unordered_set<char> miss;
+inline std::vector<std::unordered_set<char>> blow(5);  // set un correct index;
+inline std::vector<char> correct(5, ' ');
+}  // namespace _wordle_
 
 #endif
